f7cdc6bd9e049c193e6a5042bd955468c4cad0d9
cinsy26 <siusy2618@naver.com>
2025-06-03 17:54:18 +0900

    first commit

---
+++ src/test/java/com/example/portfolioback/PortfoliobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfolioback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoliobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
